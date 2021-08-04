c4c6a5db09173ed8220998fdde10d5e23c7d9852
SimpleCoder-dev <erlljhanwincatapang@gmail.com>
2021-08-04 11:41:40 +0800

    second commit

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 console.log('Hello World!');
 
 var element = document.getElementById('btnExplore');
+var all = document.getElementById('all');
 
 element.addEventListener('click',() => {
   window.scrollTo(0,650);
   
 });
+
+
+all.addEventListener('click',() => {
+  window.scrollTo(0,0);
+});
+
 
 
 
